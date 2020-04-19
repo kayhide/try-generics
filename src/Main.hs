435151f5de6fc5950984e7e9cded3c87de78c07f
Hideaki Kawai <kayhide@gmail.com>
2020-04-20 01:21:16 +0900

    Create Keyed

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,14 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE DuplicateRecordFields #-}
 module Main where
 
 -- import ClassyPrelude
 import Prelude
 
 import Data.Proxy
+import Data.Kind
 import GHC.Generics
 import GHC.TypeLits
 import Debug.Trace
@@ -21,13 +23,16 @@ import Unsafe.Coerce
 --        ('MetaCons "Point" 'PrefixI 'True)
 --        (S1
 --           ('MetaSel
---              ('Just "x") 'NoSourceUnpackedness 'SourceStrict 'DecidedStrict)
+--              ('Just "x") 'NoSourceUnpackedness 'NoSourceStrictness 'DecidedLazy)
 --           (Rec0 Double)
 --         :*: S1
 --               ('MetaSel
---                  ('Just "y") 'NoSourceUnpackedness 'SourceStrict 'DecidedStrict)
+--                  ('Just "y") 'NoSourceUnpackedness 'NoSourceStrictness 'DecidedLazy)
 --               (Rec0 Double)))
-data Point = Point { x :: !Double, y :: !Double }
+data Point = Point { x :: Double, y :: Double }
+   deriving (Show, Generic)
+
+data Position = Position { x :: !Double, y :: !Double }
    deriving (Show, Generic)
 
 -- |
@@ -37,6 +42,10 @@ data Point = Point { x :: !Double, y :: !Double }
 -- >>> rep = M1 $ M1 $ M1 (K1 1.2) :*: M1 (K1 8.3)
 -- >>> to rep :: Point
 -- Point {x = 1.2, y = 8.3}
+-- >>> to rep :: Position
+-- Position {x = 1.2, y = 8.3}
+-- >>> to rep :: (Double, Double)
+-- (1.2,8.3)
 pt :: Point
 pt = Point 1.2 8.3
 
@@ -56,35 +65,39 @@ class Keys' f where
   keys' :: f a -> [String]
 
 instance Keys' a => Keys' (D1 f a) where
-  keys' x = keys' (unsafeCoerce x :: a _b)
+  keys' (M1 x) = keys' x
 
 instance Keys' a => Keys' (C1 f a) where
-  keys' x = keys' (unsafeCoerce x :: a _b)
+  keys' (M1 x) = keys' x
 
 instance (Keys' a, Keys' b) => Keys' (a :*: b) where
   keys' (x :*: y) = keys' x <> keys' y
 
-instance KnownSymbol key => Keys' (S1 ('MetaSel ('Just key) x y z) a) where
+instance KnownSymbol key => Keys' (S1 ('MetaSel ('Just key) su ss ds) a) where
   keys' _ = [symbolVal (Proxy @key)]
 
+instance KnownSymbol key => Keys' (S1 ('MetaSel 'Nothing su ss ds) (Rec0 (Keyed key a))) where
+  keys' _ = [symbolVal (Proxy @key)]
 
 -- |
 -- >>> :kind! Append '[Bool, Int] '[]
 -- Append '[Bool, Int] '[] :: [*]
--- = '[Int, Bool]
+-- = '[Bool, Int]
 -- >>> :kind! Append '[Bool, Int] '[Char, Word]
 -- Append '[Bool, Int] '[Char, Word] :: [*]
--- = '[Int, Bool, Char, Word]
+-- = '[Bool, Int, Char, Word]
 type family Append (a :: [k]) (b :: [k]) :: [k]
 type instance Append '[] ys = ys
-type instance Append (x ': xs) ys = Append xs (x ': ys)
+type instance Append (x ': xs) ys = x ': Append xs ys
+
 
 
 -- |
--- >>> :kind RowTy (Rep Point)
--- RowTy (Rep Point) :: [(Symbol, k)]
+-- >>> :kind! RowTy (Rep Point)
+-- RowTy (Rep Point) :: [(Symbol, *)]
+-- = '[ '("x", Double), '("y", Double)]
 class Row f where
-  type RowTy f :: [(Symbol, k)]
+  type RowTy f :: [(Symbol, Type)]
 
 instance Row f => Row (D1 i f) where
   type RowTy (D1 i f) = RowTy f
@@ -95,13 +108,41 @@ instance Row f => Row (C1 i f) where
 instance (Row a, Row b) => Row (a :*: b) where
   type RowTy (a :*: b) = Append (RowTy a) (RowTy b)
 
-instance KnownSymbol key => Row (S1 ('MetaSel ('Just (key :: Symbol)) x y z) a) where
-  type RowTy (S1 ('MetaSel ('Just key) x y z) a) = '[ '(key, a) ]
+instance KnownSymbol key => Row (S1 ('MetaSel ('Just (key :: Symbol)) su ss ds) (Rec0 (a :: Type))) where
+  type RowTy (S1 ('MetaSel ('Just key) su ss ds) (Rec0 a)) = '[ '(key, a) ]
+
+instance KnownSymbol key => Row (S1 ('MetaSel 'Nothing su ss ds) (Rec0 (Keyed key a))) where
+  type RowTy (S1 ('MetaSel 'Nothing su ss ds) (Rec0 (Keyed key a))) = '[ '(key, a) ]
 
 
-data Rec (row :: [(Symbol, k)]) where
+
+-- |
+-- >>> x = Keyed @"x" @Double 3.5
+-- >>> y = Keyed @"y" @Double 4.8
+-- >>> :t RCons x (RCons y RNil)
+-- RCons x (RCons y RNil) :: Rec '[ '("x", Double), '("y", Double)]
+--
+-- >>> (x, y)
+-- (Keyed 3.5,Keyed 4.8)
+--
+-- >>> keys x
+-- ["x"]
+--
+-- >>> keys (x, y)
+-- ["x","y"]
+
+newtype Keyed (k :: Symbol) (a :: Type) = Keyed a
+  deriving (Eq, Show)
+
+instance Generic (Keyed k a) where
+  type Rep (Keyed k a) = S1 ('MetaSel ('Just k) 'NoSourceUnpackedness 'NoSourceStrictness 'DecidedLazy) (Rec0 a)
+  from (Keyed x) = M1 (K1 x)
+  to (M1 (K1 x)) = Keyed x
+
+data Rec (row :: [(Symbol, Type)]) where
   RNil :: Rec '[]
-  RCons :: f x -> Rec xs -> Rec (x ': xs)
+  RCons :: Keyed k a -> Rec xs -> Rec ('(k, a) ': xs)
+
 
 
 -- class Union l r u where
@@ -115,6 +156,7 @@ data Rec (row :: [(Symbol, k)]) where
 main :: IO ()
 main = do
   print $ from pt
-  let M1 (M1 (M1 (K1 x) :*: M1 (K1 y))) = from pt
+  let M1 (M1 (rx@(M1 (K1 x)) :*: ry@(M1 (K1 y)))) = from pt
   print $ (x, y)
+  print $ (to $ M1 $ M1 $ rx :*: ry :: Point)
   print $ keys pt
